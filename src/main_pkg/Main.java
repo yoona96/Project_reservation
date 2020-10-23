b2c00ac8139039cdc9a966036766c918eb272384
s_union93 <union93@konkuk.ac.kr>
2020-10-23 14:32:38 +0900

    -develop update

---
+++ src/main_pkg/Main.java
@@ -0,0 +1,33 @@
+package main_pkg;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public void call_menu() {
+        System.out.println("------------------------------");
+        System.out.println("              메뉴");
+        System.out.println("------------------------------");
+        System.out.println("1. 예약하기");
+        System.out.println("2. 예약 조회");
+        System.out.println("3. 예약 취소");
+        System.out.println("4. 예약 변경");
+        System.out.println("5. 프로그램 종료");
+        System.out.println("6. 도움말 출력");
+        System.out.println("7. 무결성 검사\n");
+        System.out.println("원하시는 번호를 선택하세요:");
+    }
+
+    private static boolean password(String pwd) {
+
+        if(pwd.equals("1234"))
+            return true;
+        else
+            return false;
+    }
+
+}
