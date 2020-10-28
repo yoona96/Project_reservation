8dff697fdaa2ac4c2491969faf5c0a213e382ac8
yoona <hyoona1202@naver.com>
2020-10-29 01:09:12 +0900

    reservation working..

--- src/main_pkg/Reservation.java
+++ src/main_pkg/Reservation.java
@@ -40,7 +40,7 @@ public class Reservation {
     	Scanner scanner = new Scanner(System.in);
 
     	while(true) {
-    		System.out.println("예약 접수일: " + today);
+    		System.out.println("\n예약 접수일: " + today);
     		System.out.println("예약 희망 일자와, 시간, 그리고 방문하는 인원수를 차례대로 입력하세요.");
     		System.out.print("→");
 
@@ -61,20 +61,20 @@ public class Reservation {
     			//for the format of reservation date
     			reserv_date = input_value[0];
     		}else {
-    			System.out.println("입력하신 문자열이 올바르지 않습니다. <예약 희망일> + <tab> 1개 + <시간> + <tab> 1개 + <인원수>에 알맞은 형식으로 문자열을 입력해주세요!\n");
+    			System.out.println("입력하신 문자열이 올바르지 않습니다. <예약 희망일은> yyyy-mm-dd 또는 yyyymmdd의 형식으로 입력해주세요!\n");
     			continue;
     		}
     		if(input_value[1].matches("[0-9][0-9]") || input_value[1].matches("[0-9][0-9]:00") || input_value[1].matches("[0-9][0-9]시")) {
     			//for the format of reservation time
     			reserv_time = input_value[1];
     		}else {
-    			System.out.println("입력하신 문자열이 올바르지 않습니다. <예약 희망일> + <tab> 1개 + <시간> + <tab> 1개 + <인원수>에 알맞은 형식으로 문자열을 입력해주세요!\n");
+    			System.out.println("입력하신 문자열이 올바르지 않습니다. <시간>은 hh 또는 hh:00 또는 hh시의 형식으로 입력해주세요!\n");
     			continue;
     		}
-    		if(input_value[2].matches("[0-9]{1,2}")) { //for the format of reservation count
+    		if(Integer.parseInt(input_value[2]) >=1 && Integer.parseInt(input_value[2]) <= 12) { //for the format of reservation count
     			reserv_count = input_value[2];
     		}else {
-    			System.out.println("입력하신 문자열이 올바르지 않습니다. <예약 희망일> + <tab> 1개 + <시간> + <tab> 1개 + <인원수>에 알맞은 형식으로 문자열을 입력해주세요!\n");
+    			System.out.println("입력하신 문자열이 올바르지 않습니다. 예약 가능 인원은 최소 1명, 최대 12명입니다!\n");
     			continue;
     		}
 
@@ -82,7 +82,7 @@ public class Reservation {
 
     		if((reserv_date.contentEquals(date_format.plusDays(1).toString()) || reserv_date.replace("-", "").contentEquals(date_format.plusDays(1).toString().replace("-", ""))
     			|| reserv_date.contentEquals(date_format.plusDays(2).toString()) || reserv_date.replace("-", "").contentEquals(date_format.plusDays(2).toString().replace("-", "")))
-    			&& (Integer.parseInt(sub_time) >= 10 && Integer.parseInt(sub_time) <= 20) && Integer.parseInt(reserv_count) >=1 && Integer.parseInt(reserv_count) <= 12) {
+    			&& (Integer.parseInt(sub_time) >= 10 && Integer.parseInt(sub_time) <= 20)) {
     			//check if value inputed fits data rule
     			this.date = reserv_date;
     			this.time = sub_time;
@@ -100,6 +100,7 @@ public class Reservation {
     		}
     	}
     	show_table(reserv_date, sub_time, reserv_count);
+    	scanner.close();
     }
 
     /*
@@ -117,21 +118,21 @@ public class Reservation {
     	//for tables attached, format is "(table number)-(table number)"
 
     	int tmp_count = 1; //index no. of available number of people to use table
-    	int tmp_table = 0; //table number
+    	int tmp_table; //table number
     	int tmp_time = Integer.parseInt(time) - 10;
 
     	for(tmp_table = 0; tmp_table < 20; tmp_table++) { //check if there are any table available with chosen day,time,count with availability(3rd value)
     		if(tmp[2][tmp_time][tmp_table].equals("0")) { //if number of people using table is 0
     			if(Integer.parseInt(tmp[tmp_count][tmp_time][tmp_table]) >= Integer.parseInt(count)){
-    				//compare available number or people to use table with inputed number of people
-    				if(Integer.parseInt(count) < 3 && (tmp[tmp_count][tmp_time][tmp_table]).contentEquals("4") || tmp[tmp_count][tmp_time][tmp_table].equals("6")) {
+    				//compare available number of people to use table with inputed number of people
+    				if(Integer.parseInt(count) < 3 && ((tmp[tmp_count][tmp_time][tmp_table]).equals("4") || tmp[tmp_count][tmp_time][tmp_table].equals("6"))) {
     					//if current number of people is less than 3, cannot use table for 4 or 6 people
     					continue;
-    				}else if(Integer.parseInt(count) < 4 && tmp[tmp_count][tmp_time][tmp_table].contentEquals("6")){
-    					//if current number of people is less than 4, cannot use table for 6 people
+    				}else if((Integer.parseInt(count) < 4 || Integer.parseInt(count) > 6) && tmp[tmp_count][tmp_time][tmp_table].equals("6")){
+    					//if current number of people is less than 4, and more than 6, cannot use table for 6 people
     					continue;
     				}else { //add string in available_tables
-    					available_tables.append(tmp_table);
+    					available_tables.append(tmp_table + 1);
     					available_tables.append(" ");
     				}
     			}else {
@@ -141,11 +142,11 @@ public class Reservation {
     			continue;
     		}
     	}
-    	for(attached_table = 0; attached_table < 20; attached_table += 2) { //tables can be attached
+    	for(attached_table = 0; attached_table < 20; attached_table++) { //tables can be attached
     		if(tmp[2][tmp_time][attached_table].contentEquals("0") && tmp[2][tmp_time][attached_table++].contentEquals("0")) {
 				//if attached tables are both empty
-    			if(attached_table >= 0 && attached_table < 8) {
-    				//table 1~8
+    			if(attached_table >= 0 && attached_table < 6) {
+    				//table 1~6
     				if(Integer.parseInt(count) > 2 && Integer.parseInt(count) < 5) {
     					//only more than 2, less than 5 people can use attached tables for 4 people
     					available_tables.append(attached_table);
@@ -155,7 +156,8 @@ public class Reservation {
     				}else {
     					continue;
     				}
-    			}else if (attached_table >= 8 && attached_table <16) {
+    			}else if (attached_table >= 6 && attached_table <16) {
+    				//table 7~16
     				if(Integer.parseInt(count) > 4 && Integer.parseInt(count) < 9) {
         				//only more than 4, less than 9 people can use attached tables for 8 people
     					available_tables.append(attached_table);
@@ -183,26 +185,126 @@ public class Reservation {
 
 	private void show_table(String date, String time, String count) {
 		String[] tmp_date_arr = new String[3];
-		tmp_date_arr[0] = date.substring(0, 4);
-		tmp_date_arr[1] = date.substring(5, 7);
-		tmp_date_arr[2] = date.substring(8, 10);
+		String tmp_date = date.replaceAll("-", "");
+		//replace all - from date, make it into format of yyyymmdd
+		tmp_date_arr[0] = tmp_date.substring(0, 4);
+		tmp_date_arr[1] = tmp_date.substring(4, 6);
+		tmp_date_arr[2] = tmp_date.substring(6);
 		//split inputed reservation date into yyyy mm dd format
 		
 		String avail_table = search_table(date, time, count);
-		//has no. of available tables
-
-		for(int table_num = 1; table_num <= 20; table_num++) {
-			
-			if(avail_table.contains(Integer.toString(table_num))) {
-				if(table_num < 5) {
-					
-				}
+		//has no. of available tables. same as available_tables in search table function
+		String[] every_table = null;
+		every_table = avail_table.split(" ");
+		//every available tables are in this array
+		String[] every_attached_table = new String[5];
+		//every available attached tables will be in this array
+		
+		for(int i = 0; i < every_table.length; i++) {
+			System.out.print(every_table[i] + "\t");
+		}
+		System.out.println("");
+		
+		for(int i = 0; i < every_table.length; i++) {
+			if(every_table[i].matches("[0-9]" + "-" + "[0-9]{1,2}")) {
+				int j = 0;
+				every_attached_table[j] = every_table[i].substring(0,1);
+				every_attached_table[j+1] = every_table[i].substring(2);
+				every_table[i] = "";
+				System.out.print(every_attached_table[j] + "\t");
+				System.out.print(every_attached_table[j+1] + "\t");
+				j += 2;
+			}else if(every_table[i].matches("[0-9][0-9]"+ "-" + "[0-9][0-9]")) {
+				int j = 0;
+				every_attached_table[j] = every_table[i].substring(0,2);
+				every_attached_table[j+1] = every_table[i].substring(3);
+				every_table[i] = "";
+				System.out.print(every_attached_table[j] + "t\t");
+				System.out.print(every_attached_table[j+1] + "t\t");
+				j += 2;
 			}
 		}
+		//replace attached tables from every_table array
+		//now every 'not attached' tables are in every_table array
+		//every 'attached' tables are in every_attached_table array
 		
-		System.out.println(tmp_date_arr[0] + "년 " + tmp_date_arr[1] + "월 " + tmp_date_arr[2] + "일 " + time + "시 예약 가능 좌석 현황");
-		System.out.println("--------------------------------------------------");
+		for(int i = 0; i < every_table.length; i++) {
+			System.out.print(every_table[i] + "\t");
+		}
 		System.out.println("");
+		
+		String[] table_view = new String[20];
+		for(int i = 0; i < 20; i++) {
+			table_view[i] = "0";
+		}
+		
+		//rows of current table chart
+		
+		Scanner choose_table_input = new Scanner(System.in);
+
+		while(true) {
+			
+			System.out.println("\n" + tmp_date_arr[0] + "년 " + tmp_date_arr[1] + "월 " + tmp_date_arr[2] + "일 " + time + "시 예약 가능 좌석 현황");
+			System.out.println("--------------------------------------------------");
+			
+			if(every_table != null) {
+				for(int i = 0; i < every_table.length; i++) {
+					for(int j = 0; j < 20; j++) {
+						if(!table_view[j].contains(".")) {
+							if(!every_table[i].equals(Integer.toString(j + 1))) {
+								table_view[j] = Integer.toString(j + 1);
+							}else {
+								table_view[j] = Integer.toString(j + 1) + ".";
+							}
+						}
+						System.out.print(table_view[j] + "\t");
+					}
+				}
+			}
+			System.out.println("");
+			if(every_attached_table != null) {
+				for(int k = 0; k < every_attached_table.length; k++) {
+					for(int l = 0; l < 20; l++) {
+						if(table_view[l].contains(".")) {
+							System.out.println("c");
+							continue;
+						}else {
+							System.out.println("a");
+							if(!every_attached_table[k].equals(Integer.toString(l + 1))) {
+								System.out.println("b");
+								table_view[l] = "//";
+							}else {
+								table_view[l] = Integer.toString(l + 1) + ".";
+							}
+						}
+						System.out.println(table_view[l] + "`");
+					}
+				}
+			}
+			
+			System.out.println("--------------------------------------------------\n");
+			
+			System.out.println("[01] ~ [06] : 2인용 좌석");
+			System.out.println("[07] ~ [16] : 4인용 좌석");
+			System.out.println("[17] ~ [20] : 6인용 좌석");
+			System.out.println("[//] : 예약 불가능한 좌석");
+			System.out.println("붙어있는 좌석 : (1,2) (3,4) (5,6) (7,8) (9,10) (11,12) (13,14) (15,16) (17,18) (19,20)\n");
+			System.out.println("좌석을 자동으로 할당 받으시겠습니까? (y/n) : ");
+			
+			String table_choice = choose_table_input.next();
+			
+			if(table_choice.matches("[Yy]")) {
+				choose_auto('y');
+				break;
+			}else if(table_choice.matches("[Nn]")) {
+				choose_table("");
+				break;
+			}else {
+				System.out.println("입력은 y 혹은 n만 가능합니다. 다시 입력해주세요.");
+				continue;
+			}
+		}
+		choose_table_input.close();
 	}
 
 	private boolean choose_auto(char c) {
