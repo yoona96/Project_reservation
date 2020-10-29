04674ddd3fa656b2bda0ac66f6f9ed6f702ca097
yuran792 <72340294+yuran792@users.noreply.github.com>
2020-10-30 01:28:02 +0900

    정규식 추가랑 change reserv

--- src/main_pkg/Reservation_Change.java
+++ src/main_pkg/Reservation_Change.java
@@ -8,7 +8,7 @@ public class Reservation_Change {
 
 	private String name="";
 	private String phone="";
-	private String date,time,table; //index of the old reservation
+	private String date,time,table1,table2,count; //index of the old reservation
 	int[] int_menu_num = new int[5];//menu info before changing int array
 	private ArrayList<int[]> user_inform[]=new ArrayList[3];
 	private String temp[]=new String[11];
@@ -55,15 +55,25 @@ public class Reservation_Change {
 			BufferedReader br=new BufferedReader(fr);
 			String line="";
 			String new_line="";
-
-			while((line=br.readLine())!=null) { //read file
-				if(line.trim().split("\t")[0].equals(table)&&line.trim().split("\t")[3].equals(time)) {
-						new_line+=line.trim().split("\t")[0]+"\t"+line.trim().split("\t")[1]+"\t0\t"+line.trim().split("\t")[3]+"\n";
-				}
-				else {
+			String time2=Integer.toString(Integer.parseInt(time+1));
+			
+			if(attached()) {
+				while((line=br.readLine())!=null) { //read file
+					if(line.trim().split("\t")[0].equals(table1)||line.trim().split("\t")[0].equals(table2)) {
+						if(line.trim().split("\t")[3].equals(time)||line.trim().split("\t")[3].equals(time2))
+							new_line+=line.trim().split("\t")[0]+"\t"+line.trim().split("\t")[1]+"\t0\t"+line.trim().split("\t")[3]+"\n";
+					}
 					new_line+=line+"\n";
 				}
-				
+			}
+			else {
+				while((line=br.readLine())!=null) { //read file
+					if(line.trim().split("\t")[0].equals(table1)) {
+						if(line.trim().split("\t")[3].equals(time))
+							new_line+=line.trim().split("\t")[0]+"\t"+line.trim().split("\t")[1]+"\t0\t"+line.trim().split("\t")[3]+"\n";
+					}
+					new_line+=line+"\n";
+				}
 			}
 			
 			
@@ -99,6 +109,29 @@ public class Reservation_Change {
 		return;
 	}
 	
+	private boolean attached() {
+		int i_table1=Integer.parseInt(table1);
+		int i_count=Integer.parseInt(count);
+		if(i_table1>=1&&i_table1<=5) {//2인용 좌석인데
+			if(i_count>2&&i_count<=4) //3,4명이라면
+				return true;
+			else
+				return false;
+		}
+		else if(i_table1>=7&&i_table1<=15) {//4인용 좌석인데
+			if(i_count>4&&i_count<=8) //인원수가 5~8명이라면
+				return true;
+			else
+				return false;
+		}
+		else if(i_table1>=17&&i_table1<=19) { //6인용 좌석인데
+			if(i_count>7&&i_count<=12) //인원수가 9~12명이라면
+				return true;
+			else
+				return false;
+		}
+		return false;
+	}
 	public void change_main() {
 		user_inform=rc.show_reservation_inform("Change");
 		while(true) {
@@ -121,12 +154,22 @@ public class Reservation_Change {
 			String[][][] db = fi.tb.get_day();
 			
 			for(int i =0; i<user_inform[change_day].size(); i=i+2) { // 예약은 두시간씩 묶음으로 저장되니, 예약 시작시간만 입력받도록 한다.
-				int[] pos = user_inform[change_day].get(i);	// i번째 예약정보를 담아온다.
-				int table_inform = pos[1];	// i번쨰의 예약정보의 table 번호를(3차원 배열에서 2번째 항에 해당) 저장한다.
-				int time_inform = pos[0];	//	i번째 예약정보의 시간을(3차원 배열에서 3번째 항에 해당) 저장한다.
+				int[] pos = user_inform[change_day].get(i);	// user_inform[change_day]에 있는 정보 값을 하나씩 불러온다
+				int table_inform = pos[1];	//만약 붙어있는 좌석이라면 7,8 중 7번째 테이블 값을 가져온다
+				int time_inform = pos[0];	//예약 시작 시간
+				//[][time_inform][table_infrom]형식
+				
 				String reserved_time = db[3][time_inform][table_inform];	// 예약된 정보 중 시간을 받아온다.
-				if(Integer.parseInt(reserved_time)==Integer.parseInt(time)) {
-					table=db[0][time_inform][table_inform];
+				if(Integer.parseInt(reserved_time)==Integer.parseInt(time)) { //입력받은 시간과 같다면
+					count=db[2][time_inform][table_inform];
+					if(attached()) {
+						table1=db[0][time_inform][table_inform];
+						table2=db[0][time_inform][table_inform+1];
+					}
+					else {
+						table1=db[0][time_inform][table_inform];
+					}
+					
 					name=db[4][time_inform][table_inform];
 					phone=db[5][time_inform][table_inform];
 					int_menu_num[0]=Integer.parseInt(db[6][time_inform][table_inform]);
